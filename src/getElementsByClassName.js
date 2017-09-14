f3bfe97a977e767e581b69be7ad32ef37e586be2
Jiajun Chen <jchen0318@gmail.com>
2017-09-14 00:36:17 -0700

    Complete getElementsByClassName function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,19 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  var elementArray = [];
+  var body = document.body;
+  var getElements = function (node) {
+  	if(node.classList && node.classList.contains(className)) {
+  		elementArray.push(node);
+  	}
+  	if (node.hasChildNodes()) {
+  		var  children = node.childNodes;
+  		for (let i = 0; i < children.length; i++) {
+  			getElements(children[i])
+  		};
+  	}
+  }
+  getElements(body);
+  return elementArray;
 };
