80b1205b1058be414dfcb40d13d49d9f772600e2
Jiajun Chen <jchen0318@gmail.com>
2017-09-14 23:02:24 -0700

    Complete stringifyJSON function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,46 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = '';
+
+  if(typeof(obj) === 'function' || typeof(obj) === 'undefined') {
+  	return;
+  }
+  else if(Array.isArray(obj)) {
+  	result += '[';
+  	for(let i = 0; i < obj.length; i++) {
+  		result += stringifyJSON(obj[i]);
+  		if(i !== obj.length - 1) {
+  			result += ',';
+  		}
+  	}
+  	result += ']';
+
+  }  else if (obj === null) {
+  	result += null;
+
+  } else if (typeof(obj) === 'object') {
+  	result += '{'
+  	var keyArray = Object.keys(obj);
+  	if (keyArray.length > 0) {
+
+  		for(let i = 0; i < keyArray.length; i++) {
+  			if (keyArray[i] !== 'functions' && keyArray[i] !== 'undefined') {
+  				result += stringifyJSON(keyArray[i]) + ':' + stringifyJSON(obj[keyArray[i]]);
+
+  				if(i !== keyArray.length - 1) {
+  					result += ',';
+  				}
+  			}
+  		}
+  	}
+  	
+  	result += '}';
+
+  } else if(typeof(obj) === 'string') {
+  	result += '"' + obj + '"';
+  } else {
+  	result += obj;
+  }
+  return result;
 };
